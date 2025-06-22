06f71de1528d43308e79b7cfd9fe7ab633c7e23a
Muhammad Ahmad <126343041+Muhammad-Ahmad-7@users.noreply.github.com>
2025-06-22 18:20:58 +0500

    Update page.tsx

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -9,8 +9,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
-import React from 'react'
-
 import React, { FormEvent, useState } from 'react'
 
 const Page = () => {
